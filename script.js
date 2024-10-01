db7d61e95dec34829368dc6a8a94705e78d4f797
Fazila Waseem <fazilawaseem829@gmail.com>
2024-10-01 07:21:35 +0500

    done

---
+++ script.js
@@ -0,0 +1,11 @@
+function changeBackgroundColor() {
+   
+    document.body.style.backgroundColor = `rgb(${ Math.floor(Math.random()*255)},
+      
+    ${Math.floor(Math.random() * 255 )} , ${Math.floor(Math.random() * 255 )})`;
+     
+     
+
+
+   
+}
